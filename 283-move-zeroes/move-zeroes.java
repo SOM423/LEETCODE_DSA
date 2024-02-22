31f4ef3ba6f3ed0f9870f66f4ddf72a61f860d42
SOM423 <75077641+SOM423@users.noreply.github.com>
2024-02-22 19:12:00 +0530

    Time: 1 ms (100.00%) | Memory: 45.6 MB (95.48%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int i=0;
+        int n = nums.length;
+
+        for(int j=0;j<n;j++)
+        {
+            if(nums[j] !=0)
+            {
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+
+        for(int k=i;k<n;k++)
+        {
+            nums[k]=0;
+        }
+    }
+}
